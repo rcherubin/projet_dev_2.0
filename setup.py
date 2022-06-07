5aef60ff243767dec804bb386fbcc5060c7dd5f9
MTARHOUNI <mohamedamine.tarhouni@ynov.com>
2022-06-07 17:07:40 +0200

    main

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from ssl import Options
 import cx_Freeze
 executables= [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="topDownShooter",
     options={"build_exe":{"packages":["pygame","sqlite3","sys"],
-    "include_files":[""]}},
+    "include_files":["Man 1.jpg"]}},
     executables=executables
 )
